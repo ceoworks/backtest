8e2ea9362ccbec7e773c7880e1b7c3958c379170
Stanislav Slesarev <s.slesarev@crello.com>
2019-10-13 13:42:16 +0300

    Bollinger bands and stochRSI support

--- run.js
+++ run.js
@@ -1,15 +1,25 @@
 const Backtest = require('./lib');
-const candles = require('./BTC_1H_CANDLES.json');
+const candles = require('./bitfinex_BTCUSD_15m.json');
 const {ema200Strategy} = require('./strategies');
 const drawChart = require('./lib/util/drawChart');
 
 const indicators = [
 	{name: 'rsi', period: 14},
-	{name: 'ema', period: 50},
-	{name: 'ema', period: 200},
+	{name: 'ema', period: 9},
+	{name: 'ema', period: 21},
+	{
+		name: 'stochasticrsi',
+		period: 14,
+		rsiPeriod: 14,
+		stochasticPeriod: 14,
+		kPeriod: 3,
+		dPeriod: 3,
+	},
+	// {name: 'adx', period: 14},
+	{name: 'bollingerbands', period: 14, stddev: 2},
 ];
-const takeProfit = 0.1;
-const stopLoss = 0.03;
+const takeProfit = 0.05;
+const stopLoss = 0.04;
 const backtesting = new Backtest({
 	candles, strategy: ema200Strategy, indicators, takeProfit, stopLoss,
 });
--- strategies.js
+++ strategies.js
@@ -1,18 +1,33 @@
 module.exports.ema200Strategy = function (candleIndex, candle, positionTypes, orderTypes) {
+	// const {close, candleTimestamp} = candle;
+	// const {ema9 = [], ema21 = [], rsi14 = []} = this.indicators;
+	// const lastEma50Value = ema9[candleIndex];
+	// const lastEma200Value = ema21[candleIndex];
+	// const lastRsi14Value = rsi14[candleIndex];
+	// const positionType = lastEma50Value > lastEma200Value
+	// 	? positionTypes.LONG : positionTypes.SHORT;
+	// if (lastEma50Value) {
+	// 	const price = lastEma50Value;
+	// 	const fillOrKill = true;
+	// 	this.placeOrder(
+	// 		price,
+	// 		positionType,
+	// 		orderTypes.LIMIT,
+	// 		fillOrKill,
+	// 	);
+	// }
+	const {
+		ema9 = [], ema21 = [], rsi14 = [],
+		stochasticrsi14 = [], adx14 = [],
+		bollingerbands14 = [],
+	} = this.indicators;
+
 	const {close, candleTimestamp} = candle;
-	const {ema50 = [], ema200 = []} = this.indicators;
-	const lastEma50Value = ema50[candleIndex];
-	const lastEma200Value = ema200[candleIndex];
-	const positionType = lastEma50Value > lastEma200Value
-		? positionTypes.LONG : positionTypes.SHORT;
-	if (lastEma200Value) {
-		const price = lastEma200Value;
-		const fillOrKill = true;
-		this.placeOrder(
-			price,
-			positionType,
-			orderTypes.LIMIT,
-			fillOrKill,
-		);
-	}
+	const fillOrKill = true;
+	this.placeOrder(
+		close * 0.95,
+		positionTypes.LONG,
+		orderTypes.LIMIT,
+		fillOrKill,
+	);
 };
